696d80ee4c51b1a880a3c9159e4c60fbe561c967
Nikhil <nikhil.razdan@hotmail.com>
2014-07-01 18:32:26 +0530

    Update tracepy.py

--- tracepy.py
+++ tracepy.py
@@ -1,9 +1,10 @@
 #!/bin/bash
 import os
 
-for i in range(160):
-    ip = "5.199.175.%d" % (i)
-    response = os.system("traceroute -z 10 " + ip)
+for i in range(255): # Enter range here
+    ip = "192.168.1.%d" % (i) # Enter range here
+    response = os.system("traceroute -z 10 " + ip) # traceroute with a 10 sec delay
+
     print ip
 
 
